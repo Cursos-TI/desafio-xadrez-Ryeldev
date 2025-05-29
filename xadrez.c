c818be99986a9e684215e26ed884d691599352ec
Ryeldev <ryanngmarques@gmail.com>
2025-05-29 00:18:42 +0000

    Movimento do cavalo feito!

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,7 @@ int main() {
    
     //Movimento da Torre: 5 casa para a direita (Usando o while)
     int i = 1;
-        printf("\nMovimento do Torre: \n");
+        printf("\nMovimento do Torre: \n", i);
 
     while (i <= 5) 
     {
@@ -30,9 +30,27 @@ int main() {
          i = 0;
 
         for(i = 0; i < 8; i++) {
-            printf("Direita\n");
+            printf("Esquerda\n");
         }
+
+    //Movimento do cavalo
+
+    printf("\nMovimento do cavalo\n");
+
+    for (int j = 0; j < 2; ++j) {
+
+      int i = 0;
+
+      while (i < 2){
+      printf("Baixo\n");
+
+      i++;
+      }
+      printf("Esquerda \n");
+    }
+
 
 
     return 0;
+    
 }
